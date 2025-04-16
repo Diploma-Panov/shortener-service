64e9529c70724a9470f258b0df10b9cfdc102cc8
Maksym Panov <work.maksym.panov@gmail.com>
2025-04-16 15:09:07 +0300

    Implement and test update user info endpoint

--- src/__tests__/routes/api/shrt/v0/user/users.test.ts
+++ src/__tests__/routes/api/shrt/v0/user/users.test.ts
@@ -2,7 +2,12 @@ import { createTestApplication, signupRandomUser } from '../../../../../utils/ap
 import request from 'supertest';
 import { apiRouter } from '../../../../../../routes/api/shrt/v0';
 import { Express } from 'express';
-import { UserInfoDto } from '../../../../../../dto/users';
+import { UpdateUserInfoDto, UserInfoDto } from '../../../../../../dto/users';
+import {
+    generateRandomAlphabeticalString,
+    generateRandomAlphanumericalString,
+    generateUniqueEmail,
+} from '../../../../../utils/dataUtils';
 
 const app: Express = createTestApplication(apiRouter);
 
@@ -22,6 +27,46 @@ describe('Users authenticated controller test', () => {
             lastname: lastName,
             companyName,
             email: username,
+            profilePictureUrl: null,
+        });
+    });
+
+    it('should update user info', async () => {
+        const {
+            tokens: { accessToken },
+            userId,
+        } = await signupRandomUser();
+
+        const updateDto: UpdateUserInfoDto = {
+            newCompanyName: generateRandomAlphabeticalString(20),
+            newEmail: generateUniqueEmail(),
+            newFirstname: generateRandomAlphanumericalString(20),
+            newLastname: generateRandomAlphabeticalString(20),
+        };
+        const updateRes = await request(app)
+            .patch(`/user/users/info`)
+            .set('Authorization', accessToken)
+            .send(updateDto);
+        expect(updateRes.status).toEqual(200);
+        expect(updateRes.body.payload).toEqual<UserInfoDto>({
+            id: userId,
+            firstname: updateDto.newFirstname!,
+            lastname: updateDto.newLastname,
+            companyName: updateDto.newCompanyName,
+            email: updateDto.newEmail!,
+            profilePictureUrl: null,
+        });
+
+        const dataAfterRes = await request(app)
+            .get(`/user/users/info`)
+            .set('Authorization', accessToken);
+        expect(dataAfterRes.status).toEqual(200);
+        expect(dataAfterRes.body.payload).toEqual<UserInfoDto>({
+            id: userId,
+            firstname: updateDto.newFirstname!,
+            lastname: updateDto.newLastname,
+            companyName: updateDto.newCompanyName,
+            email: updateDto.newEmail!,
             profilePictureUrl: null,
         });
     });
--- src/routes/api/shrt/v0/user/users.ts
+++ src/routes/api/shrt/v0/user/users.ts
@@ -1,18 +1,39 @@
 import { Router } from 'express';
 import { logger } from '../../../../../config/logger';
-import { UserInfoDto } from '../../../../../dto/users';
+import { UpdateUserInfoDto, UserInfoDto } from '../../../../../dto/users';
 import { AuthServiceClient } from '../../../../../components/api/AuthServiceClient';
+import { updateOrCreateUser } from '../../../../../components/dao/userDao';
 
 const authenticatedUsersRouter = Router();
 
-authenticatedUsersRouter.get('/info', async (req, res) => {
-    logger.info(`Received GET /api/shrt/v0/user/users/info`);
-    const token: string = req.headers.authorization ?? '';
-    const payload: UserInfoDto = await AuthServiceClient.getPersonalInfo(token);
-    res.json({
-        payloadType: 'UserInfoDto',
-        payload,
-    });
+authenticatedUsersRouter.get('/info', async (req, res, next) => {
+    try {
+        logger.info(`Received GET /api/shrt/v0/user/users/info`);
+        const token: string = req.headers.authorization ?? '';
+        const payload: UserInfoDto = await AuthServiceClient.getPersonalInfo(token);
+        res.json({
+            payloadType: 'UserInfoDto',
+            payload,
+        });
+    } catch (e) {
+        next(e);
+    }
+});
+
+authenticatedUsersRouter.patch('/info', async (req, res, next) => {
+    try {
+        const dto: UpdateUserInfoDto = req.body;
+        logger.info(`Received PATCH /api/shrt/v0/users/info, with dto=${JSON.stringify(dto)}`);
+        const accessToken = req.headers.authorization ?? '';
+        const payload: UserInfoDto = await AuthServiceClient.updateUserInfo(accessToken, dto);
+        await updateOrCreateUser(payload);
+        res.json({
+            payloadType: 'UserInfoDto',
+            payload,
+        });
+    } catch (e) {
+        next(e);
+    }
 });
 
 export { authenticatedUsersRouter };
