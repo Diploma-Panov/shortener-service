1b5482121698941364b0a7ad6b04a756fa5aed05
Maksym Panov <work.maksym.panov@gmail.com>
2025-04-18 23:36:53 +0300

    Implement and test invite organization member endpoint

--- src/__tests__/routes/api/shrt/v0/user/organizationMembers.test.ts
+++ src/__tests__/routes/api/shrt/v0/user/organizationMembers.test.ts
@@ -7,7 +7,17 @@ import {
 import request from 'supertest';
 import { apiRouter } from '../../../../../../routes/api/shrt/v0';
 import { Express } from 'express';
-import { OrganizationMembersListDto } from '../../../../../../dto/organizationMembers';
+import {
+    InviteMemberDto,
+    OrganizationMemberDto,
+    OrganizationMembersListDto,
+} from '../../../../../../dto/organizationMembers';
+import {
+    generateRandomAlphabeticalString,
+    generateRandomAlphanumericalString,
+    generateUniqueEmail,
+} from '../../../../../utils/dataUtils';
+import { MemberRole } from '../../../../../../auth/common';
 
 const app: Express = createTestApplication(apiRouter);
 
@@ -80,5 +90,58 @@ describe('Authenticated organization members test', () => {
             perPage: 2,
         });
         expect(res4.body.payload.entries).toHaveLength(0);
+    });
+
+    it('should invite new member into the organization', async () => {
+        const {
+            tokens: { accessToken: oldToken },
+        } = await signupRandomUser();
+
+        const {
+            tokens: { accessToken },
+            organization: { slug },
+        } = await createOrganizationForUser(oldToken);
+
+        const dto: InviteMemberDto = {
+            allowedAllUrls: false,
+            allowedUrls: [1, 2, 3],
+            email: generateUniqueEmail(),
+            firstname: generateRandomAlphabeticalString(20),
+            lastname: generateRandomAlphanumericalString(20),
+            roles: [
+                MemberRole.ORGANIZATION_MEMBER,
+                MemberRole.ORGANIZATION_URLS_MANAGER,
+                MemberRole.ORGANIZATION_MEMBERS_MANAGER,
+            ],
+        };
+        const resInvited = await request(app)
+            .post(`/user/organizations/${slug}/members`)
+            .set('Authorization', accessToken)
+            .send(dto);
+        expect(resInvited.status).toEqual(200);
+        expect(resInvited.body.payload).toEqual({
+            message: 'SUCCESS',
+        });
+
+        const resMembers = await request(app)
+            .get(`/user/organizations/${slug}/members`)
+            .set('Authorization', accessToken);
+        expect(resMembers.status).toEqual(200);
+        expect(resMembers.body.payload).toEqual<OrganizationMembersListDto>({
+            entries: expect.arrayContaining<OrganizationMemberDto>([
+                {
+                    id: expect.any(Number),
+                    fullName: dto.firstname + ' ' + dto.lastname,
+                    email: dto.email,
+                    roles: expect.arrayContaining(dto.roles),
+                    allowedUrls: expect.arrayContaining(dto.allowedUrls),
+                    allowedAllUrls: dto.allowedAllUrls,
+                },
+            ]),
+            total: 2,
+            hasMore: false,
+            page: 0,
+            perPage: 10,
+        });
     });
 });
--- src/routes/api/shrt/v0/user/organizationMembers.ts
+++ src/routes/api/shrt/v0/user/organizationMembers.ts
@@ -1,8 +1,13 @@
 import { Router, Request } from 'express';
 import { logger } from '../../../../../config/logger';
 import { parseJwtToken } from '../../../../../auth/jwt';
-import { OrganizationMembersListDto } from '../../../../../dto/organizationMembers';
+import {
+    InviteMemberDto,
+    OrganizationMembersListDto,
+} from '../../../../../dto/organizationMembers';
 import { AuthServiceClient } from '../../../../../components/api/AuthServiceClient';
+import { AbstractResponseDto } from '../../../../../dto/common/AbstractResponseDto';
+import { MessageResponseDto } from '../../../../../dto/common/MessageResponseDto';
 
 const authenticatedOrganizationMembersRouter = Router({ mergeParams: true });
 
@@ -11,7 +16,7 @@ authenticatedOrganizationMembersRouter.get(
     async (
         req: Request<
             { slug: string },
-            unknown,
+            AbstractResponseDto<OrganizationMembersListDto>,
             void,
             { p?: number; q?: number; dir?: string; sb?: string }
         >,
@@ -30,6 +35,36 @@ authenticatedOrganizationMembersRouter.get(
                 await AuthServiceClient.getOrganizationMembers(accessToken, slug, query);
             res.json({
                 payloadType: 'OrganizationMembersListDto',
+                payload,
+            });
+        } catch (e) {
+            next(e);
+        }
+    },
+);
+
+authenticatedOrganizationMembersRouter.post(
+    '/',
+    async (
+        req: Request<{ slug: string }, AbstractResponseDto<MessageResponseDto>, InviteMemberDto>,
+        res,
+        next,
+    ) => {
+        try {
+            const { slug } = req.params;
+            const dto: InviteMemberDto = req.body;
+            const accessToken: string = req.headers.authorization ?? '';
+            const { userId } = parseJwtToken(accessToken);
+            logger.info(
+                `Received POST /api/shrt/v0/user/organizations/${slug}/members by userId=${userId}`,
+            );
+            const payload: MessageResponseDto = await AuthServiceClient.inviteNewOrganizationMember(
+                accessToken,
+                slug,
+                dto,
+            );
+            res.json({
+                payloadType: 'MessageResponseDto',
                 payload,
             });
         } catch (e) {
